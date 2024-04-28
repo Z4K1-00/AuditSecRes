fc9c672de034fea191efbcd96f804bbd1b985553
ZAKARIA BENDRIOUICH <zakaria.bendriouich00@gmail.com>
2024-04-28 03:11:31 +0200

    Updated

--- decap.py
+++ decap.py
@@ -2,23 +2,15 @@
 from scapy.all import *
 
 def decapsulate_and_forward(pkt):
+    # Check if it's an ICMP packet with payload
     if ICMP in pkt and pkt[ICMP].type == 8 and pkt.haslayer(Raw):
-        # Reassemble fragmented packets
-        reassembled_packet = defragment(pkt)
+        # Extract the payload (original packet) from the ICMP packet
+        original_packet = pkt[Raw].load
 
-        if reassembled_packet:
-            # Extract the payload (original packet) from the reassembled packet
-            original_payload = b""
-            for frag in reassembled_packet:
-                if Raw in frag:
-                    original_payload += frag[Raw].load
-
-            # Craft the original packet
-            original_packet = Ether() / IP() / original_payload
-
-            # Forward the decapsulated packet to the canal interface
-            sendp(original_packet, iface='canal')
+        # Forward the decapsulated packet to the canal interface
+        sendp(original_packet, iface='canal')
 
 # Sniff ICMP packets on the interface and decapsulate and forward them
 sniff(iface='h3-eth0', prn=decapsulate_and_forward, filter='icmp')
+
 
--- forward.py
+++ forward.py
@@ -4,11 +4,9 @@ from scapy.all import *
 
 def forward_packet(packet):
 	# Craft ICMP Echo Request packet with the encapsulated original packet
-	icmp_packet = IP(src="192.168.10.1", dst="192.168.10.3") / ICMP(type="echo-request") / packet[IP]
+	icmp_packet = IP(src="192.168.10.1", dst="192.168.10.3") / ICMP(type="echo-request") / packet
 	# Send the encapsulated packet to the destination IP address
-	frags = fragment(icmp_packet, fragsize=1500)
-	for frag in frags :
-		send(frag, iface='h1-eth0')
+	send(icmp_packet, iface='h1-eth0')
 
 # Sniff packets on the canal interface and forward them
 sniff(iface='canal', prn=forward_packet)
